aff5ab4d5b1645cd5539acb27663a314097f189f
holgac <huseyinolgac@gmail.com>
2024-01-28 01:16:46 +0300

    pause button

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -8,3 +8,8 @@ func on_score_updated(game: GDGame):
   for player in game.scores:
     var label: Label = get_node(player + 'Score');
     label.set_text(str(game.scores[player]))
+
+
+func _on_pause_button_toggled(toggled_on):
+  print('is toggled?', toggled_on)
+  get_tree().set_pause(toggled_on)
