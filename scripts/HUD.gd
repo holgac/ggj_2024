36296549bc50de1c8286b3f6e8e172ce4f18a9e3
holgac <huseyinolgac@gmail.com>
2024-01-28 16:43:35 +0300

    rest of stuff

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name GDHud;
 
+var time_left = 300;
+
 func _ready():
   pass
 
@@ -13,3 +15,18 @@ func on_score_updated(game: GDGame):
 func _on_pause_button_toggled(toggled_on):
   print('is toggled?', toggled_on)
   get_tree().set_pause(toggled_on)
+
+
+func _on_second_timer_timeout():
+  if time_left <= 0:
+    Session.goto_scene(Session.MainMenuScene);
+  time_left -= 1;
+  var lbl: Label = get_node('TopUI_Frame/GameCountdown');
+  if time_left < 60:
+    lbl.set_text(str(time_left))
+  else:
+    var minute = int(time_left / 60)
+    var second = time_left % 60;
+    lbl.set_text(str(minute) + ':' + str(second))
+    
+  pass # Replace with function body.
